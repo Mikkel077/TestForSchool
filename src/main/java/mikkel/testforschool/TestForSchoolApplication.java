8a22221da208bd87fdb0a1386d149bfdd4b97ad5
mikkel077 <mikkelhansen077@gmail.com>
2024-10-24 11:01:40 +0200

    init

---
+++ src/main/java/mikkel/testforschool/TestForSchoolApplication.java
@@ -0,0 +1,13 @@
+package mikkel.testforschool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestForSchoolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestForSchoolApplication.class, args);
+    }
+
+}
---
+++ src/test/java/mikkel/testforschool/TestForSchoolApplicationTests.java
@@ -0,0 +1,13 @@
+package mikkel.testforschool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestForSchoolApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
